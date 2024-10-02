00d68b0404a2f023719e349593e3270dee460e84
jicadz <cadiz.joshuaiangit2011@gmail.com>
2024-10-02 15:24:01 +0800

    second commit

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const { getConnectedClient } = require("../database");
 
 const getCollection = () => {
   const client = getConnectedClient();
-  const collection = client.db("todosdb").collection("todos");
+  const collection = client.db("todosdbtwo").collection("todostwo");
   return collection;
 };
 
